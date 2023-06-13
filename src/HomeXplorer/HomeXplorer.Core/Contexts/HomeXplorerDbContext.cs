b981c91f55eb7e6a7c4e128373ace6696a1a5c91
Dilyan-Iliev <dilqnn.ilievv@abv.bg>
2023-06-13 11:16:23 +0200

    Initial Create

---
+++ src/HomeXplorer/HomeXplorer.Core/Contexts/HomeXplorerDbContext.cs
@@ -0,0 +1,13 @@
+﻿namespace HomeXplorer.Core.Contexts
+{
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore;
+
+    public class HomeXplorerDbContext : IdentityDbContext
+    {
+        public HomeXplorerDbContext(DbContextOptions<HomeXplorerDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ src/HomeXplorer/HomeXplorer/Program.cs
@@ -0,0 +1,60 @@
+namespace HomeXplorer
+{
+    using HomeXplorer.Core.Contexts;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.EntityFrameworkCore;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            var services = builder.Services;
+
+            // Add services to the container.
+            var connectionString = builder.Configuration.GetConnectionString("MSSQL")
+                ?? throw new InvalidOperationException("Connection string 'MSSQL' not found.");
+
+            services.AddDbContext<HomeXplorerDbContext>(options =>
+                options.UseSqlServer(connectionString));
+            services.AddDatabaseDeveloperPageExceptionFilter();
+
+            services.AddDefaultIdentity<IdentityUser>(options =>
+            {
+                options.SignIn.RequireConfirmedAccount = true;
+            })
+                .AddEntityFrameworkStores<HomeXplorerDbContext>();
+            services.AddControllersWithViews();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseMigrationsEndPoint();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+            app.MapRazorPages();
+
+            app.Run();
+        }
+    }
+}
