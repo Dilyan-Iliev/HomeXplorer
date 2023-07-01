2b33794a722dc5a6ff92220d7a688a92a1e1fc88
Dilyan-Iliev <dilqnn.ilievv@abv.bg>
2023-07-01 18:37:09 +0200

    Add personal profile page

---
+++ src/HomeXplorer/HomeXplorer.ViewModels/Profile/AgentProfileViewModel.cs
@@ -0,0 +1,21 @@
+﻿namespace HomeXplorer.ViewModels.Profile
+{
+    public class AgentProfileViewModel
+    {
+        public string FullName { get; set; } = null!;
+
+        public string City { get; set; } = null!;
+
+        public string Country { get; set; } = null!;
+
+        public string Email { get; set; } = null!;
+
+        public string PhoneNumber { get; set; } = null!;
+
+        public string? PersonalImage { get; set; } = null!; //add default picture when they dont have one
+
+        public int TotalUploadedProperties { get; set; }
+
+        
+    }
+}
---
+++ src/HomeXplorer/HomeXplorer/Areas/Agent/Controllers/ProfileController.cs
@@ -0,0 +1,14 @@
+﻿namespace HomeXplorer.Areas.Agent.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    public class ProfileController 
+        : BaseAgentController
+    {
+        [HttpGet]
+        public IActionResult MyProfile()
+        {
+            return this.View();
+        }
+    }
+}
