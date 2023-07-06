5b3d094b334d1685a6d5ddf58c4a15a851c2e0b8
Dilyan-Iliev <dilqnn.ilievv@abv.bg>
2023-07-06 11:28:53 +0200

    Add Renter area

---
+++ src/HomeXplorer/HomeXplorer/Areas/Renter/Controllers/BaseRenterController.cs
@@ -0,0 +1,13 @@
+﻿namespace HomeXplorer.Areas.Renter.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Authorization;
+
+    using static HomeXplorer.Common.UserRoleConstants;
+
+    [Area(Renter)]
+    [Authorize(Roles = Renter)]
+    public class BaseRenterController : Controller
+    {
+    }
+}
---
+++ src/HomeXplorer/HomeXplorer/Areas/Renter/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿namespace HomeXplorer.Areas.Renter.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    public class HomeController : BaseRenterController
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
