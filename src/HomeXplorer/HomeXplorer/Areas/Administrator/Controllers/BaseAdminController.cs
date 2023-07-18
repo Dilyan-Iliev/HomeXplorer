b46804bcab02a9bde136d9c45eb8af2fe54c35bc
Dilyan-Iliev <dilqnn.ilievv@abv.bg>
2023-07-18 14:47:34 +0200

    Integrated admin area with layout

---
+++ src/HomeXplorer/HomeXplorer/Areas/Administrator/Controllers/BaseAdminController.cs
@@ -0,0 +1,13 @@
+﻿namespace HomeXplorer.Areas.Administrator.Controllers
+{
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+
+    using static HomeXplorer.Common.UserRoleConstants;
+
+    [Area(Administrator)]
+    [Authorize(Roles = Administrator)]
+    public class BaseAdminController : Controller
+    {
+    }
+}
---
+++ src/HomeXplorer/HomeXplorer/Areas/Administrator/Controllers/DashboardController.cs
@@ -0,0 +1,15 @@
+﻿namespace HomeXplorer.Areas.Administrator.Controllers
+{
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+
+    [AllowAnonymous]
+    [Area("Administrator")]
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
