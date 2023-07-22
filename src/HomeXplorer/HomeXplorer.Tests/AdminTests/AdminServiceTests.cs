cf3cae4bca461aa2a07ce991eeeed44045baa480
Dilyan-Iliev <dilqnn.ilievv@abv.bg>
2023-07-22 14:19:01 +0200

    Add more unit tests for admin service

--- src/HomeXplorer/HomeXplorer.Tests/AdminTests/AdminServiceTests.cs
+++ src/HomeXplorer/HomeXplorer.Tests/AdminTests/AdminServiceTests.cs
@@ -6,27 +6,46 @@
 
     using HomeXplorer.Data.Entities;
     using HomeXplorer.Core.Contexts;
+    using HomeXplorer.ViewModels.Admin;
     using HomeXplorer.Core.Repositories;
     using HomeXplorer.Services.Contracts;
     using HomeXplorer.Services.Interfaces;
-    using HomeXplorer.ViewModels.Admin;
 
     public class AdminServiceTests
     {
-        private static DbContextOptions<HomeXplorerDbContext> GetInMemoryDbOptions(string databaseName)
+        private DbContextOptions<HomeXplorerDbContext> options;
+        private HomeXplorerDbContext dbContext;
+
+        [SetUp]
+        public void SetUp()
         {
-            return new DbContextOptionsBuilder<HomeXplorerDbContext>()
-                .UseInMemoryDatabase(databaseName)
+            options = new DbContextOptionsBuilder<HomeXplorerDbContext>()
+                .UseInMemoryDatabase(databaseName: "TestDataBase")
                 .Options;
+
+            dbContext = new HomeXplorerDbContext(options);
+        }
+
+        [Test]
+        public void Admin_Service_Should_Be_Successfully_Initialized_With_Its_Dependencies()
+        {
+            //Arrange
+            var mockedCountryService = new Mock<ICountryService>();
+            var mockedRepo = new Mock<IRepository>();
+
+            //Act
+            var adminService = new AdminService(mockedRepo.Object, mockedCountryService.Object);
+
+            //Assert
+            Assert.That(adminService, Is.Not.Null);
         }
 
         [Test]
         public async Task Get_All_Building_Types_Method_Should_Return_Correct_Data()
         {
-            var options = GetInMemoryDbOptions("TestDataBase");
+            //Arrange
+            SetUp();
 
-            // Set up an in-memory database context with the options
-            var context = new HomeXplorerDbContext(options);
             var buildingTypes = new List<BuildingType>
             {
                 new BuildingType() {Name = "Luxury"},
@@ -35,21 +54,22 @@
             };
 
             // Seed the in-memory database with test data
-            context.BuildingTypes.AddRange(buildingTypes);
-            await context.SaveChangesAsync();
+            dbContext.BuildingTypes.AddRange(buildingTypes);
+            await dbContext.SaveChangesAsync();
 
             var mockedCountryService = new Mock<ICountryService>();
 
             // Use the real repository with the in-memory database context
-            var repository = new Repository(context);
+            var repository = new Repository(dbContext);
 
             var adminService = new AdminService(repository, mockedCountryService.Object);
 
             // Act
             var result = await adminService.GetAllBuildingTypesAsync();
+
+            // Assert
             Assert.Multiple(() =>
             {
-                // Assert
                 Assert.That(result, Is.Not.EqualTo(null));
                 Assert.That(result,
                     Is.EqualTo(buildingTypes.Select(bt => bt.Name)));
@@ -60,9 +80,8 @@
         public async Task Add_New_Building_Type_Method_Should_Return_True_If_DB_Already_Has_The_Building_Type()
         {
             //Arrange
-            var options = GetInMemoryDbOptions("TestDataBase");
+            SetUp();
 
-            var context = new HomeXplorerDbContext(options);
             var buildingTypes = new List<BuildingType>
             {
                 new BuildingType() {Name = "Test"},
@@ -70,13 +89,13 @@
                 new BuildingType() {Name = "Test2"},
             };
 
-            context.BuildingTypes.AddRange(buildingTypes);
-            await context.SaveChangesAsync();
+            dbContext.BuildingTypes.AddRange(buildingTypes);
+            await dbContext.SaveChangesAsync();
 
             var mockedCountryService = new Mock<ICountryService>();
 
             // Use the real repository with the in-memory database context
-            var repository = new Repository(context);
+            var repository = new Repository(dbContext);
 
             //Act
             var adminService = new AdminService(repository, mockedCountryService.Object);
@@ -89,7 +108,130 @@
 
             var result = await adminService.AddNewBuildingTypeAsync(buildingTypeModel);
 
-            Assert.IsTrue(result);
+            Assert.That(result, Is.True);
+        }
+
+        [Test]
+        public async Task Add_New_Building_Type_Method_Should_Return_False_If_DB_Does_Not_Have_The_Building_Type()
+        {
+            //Arrange
+            SetUp();
+
+            var buildingTypes = new List<BuildingType>
+            {
+                new BuildingType() {Name = "Test"},
+                new BuildingType() {Name = "Average"},
+                new BuildingType() {Name = "Test2"},
+            };
+
+            dbContext.BuildingTypes.AddRange(buildingTypes);
+            await dbContext.SaveChangesAsync();
+
+            var mockedCountryService = new Mock<ICountryService>();
+
+            // Use the real repository with the in-memory database context
+            var repository = new Repository(dbContext);
+
+            var adminService = new AdminService(repository, mockedCountryService.Object);
+
+            var buildingTypeViewModel = new AddNonExistingBuildingTypeViewModel()
+            {
+                Name = "Ordinary"
+            };
+
+            //Act
+            var result = await adminService.AddNewBuildingTypeAsync(buildingTypeViewModel);
+
+            //Assert
+            Assert.That(result, Is.False);
+        }
+
+        [Test]
+        public async Task Add_New_Building_Type_Method_Should_Successfully_Add_New_Building_Type()
+        {
+            //Arrange
+            SetUp();
+
+            var mockedCountryService = new Mock<ICountryService>();
+
+            // Use the real repository with the in-memory database context
+            var repository = new Repository(dbContext);
+
+            var adminService = new AdminService(repository, mockedCountryService.Object);
+
+            var buildingTypeViewModel = new AddNonExistingBuildingTypeViewModel()
+            {
+                Name = "Ordinary"
+            };
+
+            //Act
+            await adminService.AddNewBuildingTypeAsync(buildingTypeViewModel);
+
+            //Assert
+            Assert.That(await dbContext.BuildingTypes.AnyAsync(x => x.Name == buildingTypeViewModel.Name),
+                Is.True);
+        }
+
+        [Test]
+        public async Task Add_New_Country_Method_Should_Return_True_If_DB_Alread_Has_The_Country()
+        {
+            //Arrange
+            SetUp();
+
+            var countries = new[]
+            {
+                new Country() { Name = "Test"},
+                new Country() { Name = "Sofia"}
+            };
+
+            dbContext.Countries.AddRange(countries);
+            await dbContext.SaveChangesAsync();
+
+            var mockedCountryService = new Mock<ICountryService>();
+            var repository = new Repository(dbContext);
+
+            var adminService = new AdminService(repository, mockedCountryService.Object);
+
+            var countryModel = new AddNonExistingCountryViewModel()
+            {
+                Name = "Sofia"
+            };
+
+            //Act
+            var result = await adminService.AddNewCountryAsync(countryModel);
+
+            //Assert
+            Assert.That(result, Is.True);
+        }
+
+        [Test]
+        public async Task Add_New_Country_Method_Should_Return_False_If_DB_Does_Not_Have_The_Country()
+        {
+            //Arange
+            SetUp();
+
+            var mockedCountryService = new Mock<ICountryService>();
+            var repository = new Repository(dbContext);
+
+            var adminService = new AdminService(repository, mockedCountryService.Object);
+
+            var countryModel = new AddNonExistingCountryViewModel()
+            {
+                Name = "Test"
+            };
+
+            //Act
+            var result = await adminService.AddNewCountryAsync(countryModel);
+
+            //Assert
+            Assert.That(result, Is.False);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            dbContext.Database.EnsureDeleted();
+            //dbContext.Dispose();
         }
     }
 }
