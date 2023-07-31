d5b09cb7fc19e488cf7b8f296a0f783922a408d4
Dilyan-Iliev <dilqnn.ilievv@abv.bg>
2023-07-31 08:11:07 +0200

    Add more tests for admin dashboard controller

--- src/HomeXplorer/HomeXplorer.Tests/AdminTests/AdminDashboardControllerTests.cs
+++ src/HomeXplorer/HomeXplorer.Tests/AdminTests/AdminDashboardControllerTests.cs
@@ -3,6 +3,8 @@
     using HomeXplorer.Areas.Administrator.Controllers;
     using HomeXplorer.Services.Contracts;
     using HomeXplorer.ViewModels.Admin;
+    using HomeXplorer.ViewModels.City;
+    using HomeXplorer.ViewModels.Country;
     using Microsoft.AspNetCore.Mvc;
     using Moq;
 
@@ -57,6 +59,73 @@
 
             var viewResult = result as ViewResult;
             Assert.That(expectedDashboardStatistics, Is.EqualTo(viewResult!.Model));
+        }
+
+        //[Test]
+        //public async Task Index_Returns_TempDataView_Method_On_Exception()
+        //{
+        //    adminService.Setup(a => a.GetDashboardInfoAsync())
+        //.ThrowsAsync(new Exception("Simulated exception"));
+
+        //    var adminController =
+        //        new DashboardController(adminService.Object, countryService.Object, agentPropertyService.Object);
+
+        //    // Act
+        //    var result = await adminController.Index();
+
+        //    // Assert
+        //    Assert.That(result, Is.Not.Null);
+        //    Assert.That(result, Is.TypeOf<ViewResult>());
+
+        //    var viewResult = result as ViewResult;
+        //    Assert.AreEqual("TempDataView", viewResult.ViewName);
+        //}
+
+        [Test]
+        public async Task All_Countries_Should_Return_Correct_Data_When_No_Exceptions_Occure()
+        {
+            //Arrange
+            var expectedModel = new AllCountriesWithCitiesViewModel()
+            {
+                Countries = new List<SelectCountryViewModel>()
+                {
+                    new SelectCountryViewModel() { Id = 1, Name = "TestCountry"}
+                },
+                Cities = new List<SelectCityViewModel>()
+                {
+                    new SelectCityViewModel() { Id = 1, Name = "TestCity1"},
+                    new SelectCityViewModel() { Id = 2, Name = "TestCity2"},
+                }
+            };
+
+            adminService.Setup(a => a.GetAllCountriesAsync())
+                .ReturnsAsync(expectedModel);
+
+            var adminController =
+                new DashboardController(adminService.Object, countryService.Object, agentPropertyService.Object);
+
+            //Act
+            var result = await adminController.AllCountries();
+
+            //Assert
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result, Is.TypeOf<ViewResult>());
+
+            var viewResult = result as ViewResult;
+            Assert.That(expectedModel, Is.EqualTo(viewResult!.Model));
+
+            var model = viewResult.Model as AllCountriesWithCitiesViewModel;
+            Assert.Multiple(() =>
+            {
+                Assert.That(model!.Countries, Is.Not.Null);
+                Assert.That(model.Countries.Count(), Is.GreaterThan(0));
+                Assert.That(model.Countries.Count(), Is.EqualTo(1));
+
+                Assert.That(model.Cities, Is.Not.Null);
+                Assert.That(model.Cities.Count(), Is.GreaterThan(0));
+                Assert.That(model.Cities.Count(), Is.EqualTo(2));
+            });
+
         }
     }
 }
