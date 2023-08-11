ef66ae55a4b225605a74e8aeb4fb47bb6d368c8e
Dilyan-Iliev <dilqnn.ilievv@abv.bg>
2023-08-11 13:16:51 +0200

    Add new tempdata error message

--- src/HomeXplorer/HomeXplorer/Controllers/HomeController.cs
+++ src/HomeXplorer/HomeXplorer/Controllers/HomeController.cs
@@ -74,7 +74,7 @@
             catch (Exception)
             {
                 this.TempData["UnexpectedError"] = "Something went wrong, please try again";
-                return this.View();
+                return this.RedirectToAction(nameof(Index));
             }
         }
 
